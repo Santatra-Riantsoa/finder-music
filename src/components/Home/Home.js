423ef1ae8ac3ee8ee3fc02f2cf30562c857fa1a0
Riantsoa <santatrariantsoa@gmail.com>
2020-03-22 19:21:01 +0400

    commit artist,album,member

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react'
+    import React, { Component } from 'react'
 import { Paper, Tabs, Tab } from '@material-ui/core'
 import TabPanel from '../Util/TabPanel'
 import { Link } from 'react-router-dom';
 import { get } from '../../util/util';
+import metallica from "../../data/mettalica";
+import Artist from './Artist/Artist';
+import Albums from './Albums/Albums';
+import Song from './Song/Song';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value : 0
+            artist : metallica,
+            value : 0,
+            currentAlbum : null
         }
 
         this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
-        get("https://wasabi.i3s.unice.fr/search/dbinfo").then(response=>{
-            console.log(response);
+        get('https://wasabi.i3s.unice.fr/search/more/Metallica').then(response=>{
         })
     }
-
+    seeSong(event,album){
+                console.log("itooooooo   ",this);
+               this.setState({currentAlbum : album });
+                this.handleChange(event,2);
+    }
     handleChange(event, newValue) {
         this.setState({value:newValue});
     }
@@ -41,14 +50,15 @@ export default class Home extends Component {
                         <Tab label="Songs" />
                     </Tabs>
                     <TabPanel value={value} index={0}>
-                        Artist: link to detail :
+                        <Artist {...this.state.artist}
+                        ></Artist>
                         <Link to="/detail/id">Go to detail</Link>
                     </TabPanel>
                     <TabPanel value={value} index={1}>
-                        Album
+                        <Albums albums={this.state.artist.albums} clicked={(event)=>this.seeSong(event,this.state.artist.albums[0])}></Albums>
                     </TabPanel>
                     <TabPanel value={value} index={2}>
-                        Songs
+                        <Song album={this.state.currentAlbum} ></Song>
                     </TabPanel>
                 </Paper>
             </div>
